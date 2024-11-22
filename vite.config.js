2ab21d4c5547299973ceffede3e54be890c68470
Steveol2 <stefanoletu@gmail.com>
2024-11-22 03:45:58 -0800

    Fix case-sensitive file path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,8 +14,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      // Optional: Resolve aliases for imports if needed
-      "@": "/src",
+      "@": path.resolve(__dirname, "src"), // Correct alias for src folder
     },
   },
 });
